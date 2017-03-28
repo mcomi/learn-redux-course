9895f5cbf3ba6d221ba606a98b6f75a88ce4ee65
Manuel Comi <mcomi@hotmail.com>
2017-03-28 12:24:36 -0600

    adding actions, reducers and store

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -7,5 +7,23 @@ import css from './styles/style.styl';
 
 // import components
 import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
 
-render(<Main/>, document.getElementById('root'));
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+const router = (
+  <Provider store={store}>
+
+    <Router history={history}>
+      <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+  </Provider>
+)
+
+render(router, document.getElementById('root'));
